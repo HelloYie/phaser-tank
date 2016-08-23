e20bcb6fefa9894c9f24fd756a5ecf73df2aee2d
iawia002 <z2d@jifangcheng.com>
2016-08-23 15:53:47 +0800

    同步死亡

--- server/socket.js
+++ server/socket.js
@@ -4,50 +4,46 @@ const util = require('util');
 const path = require('path');
 const io = require('socket.io');
 const Player = require('./player');
+
 const port = process.env.PORT || 8080;
 
 let socket;	// Socket controller
 let players;	// object of connected players
 
-module.exports = function init (server) {
-  // Create an empty object to store players
-  players = {};
-
-  // Attach Socket.IO to server
-  socket = io.listen(server);
-
-  // Start listening for events
-  setEventHandlers();
-}
 
 /* ************************************************
 ** GAME EVENT HANDLERS
 ************************************************ */
 
-const setEventHandlers = function () {
-  // Socket.IO
-  socket.sockets.on('connection', onSocketConnection);
+function playerById(id, silence) {
+  const playerObj = players[id];
+  if (playerObj) {
+    return playerObj;
+  }
+  if (!silence) {
+    util.log('Player not found: ', id);
+  }
+  return false;
 }
 
-// New socket connection
-function onSocketConnection (client) {
-  util.log('New player has connected: ' + client.id);
+function onKill(data) {
+  console.log(data);
+  const removePlayer = playerById(data.id);
+  // Player not found
+  if (!removePlayer) {
+    return;
+  }
 
-  // Listen for client disconnected
-  client.on('disconnect', onClientDisconnect);
-
-  // Listen for new player message
-  client.on('new player', onNewPlayer);
-
-  // Listen for move player message
-  client.on('move player', onMovePlayer);
-
-  // Listen for shot
-  client.on('shot', onShot);
+  // removePlayer.player.kill();
+  console.log(removePlayer);
+  // Remove player from array
+  delete players[data.id];
+  this.broadcast.emit('kill', { id: data.id });
 }
 
-function onClientDisconnect () {
-  util.log('Player has disconnected: ' + this.id);
+
+function onClientDisconnect() {
+  util.log(`Player has disconnected: ${this.id}`);
 
   const removePlayer = playerById(this.id);
 
@@ -56,18 +52,19 @@ function onClientDisconnect () {
   }
   delete players[this.id];
 
-  this.broadcast.emit('remove player', {id: this.id});
+  this.broadcast.emit('remove player', { id: this.id });
 }
 
-function onNewPlayer (data) {
+function onNewPlayer(data) {
   const newPlayer = new Player(data.x, data.y, data.angle, data.name);
-  let self = this;
+  const self = this;
   newPlayer.id = this.id;
 
-  this.broadcast.emit('new player', {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY(), name: newPlayer.getName()});
+  this.broadcast.emit('new player', { id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY(), name: newPlayer.getName() });
 
-  let i, existingPlayer;
-  Object.keys(players).forEach(function(playerId){
+  // let i;
+  let existingPlayer;
+  Object.keys(players).forEach((playerId) => {
     existingPlayer = players[playerId];
     self.emit(
       'new player',
@@ -83,8 +80,7 @@ function onNewPlayer (data) {
   players[this.id] = newPlayer;
 }
 
-function onMovePlayer (data) {
-
+function onMovePlayer(data) {
   const movePlayer = playerById(this.id);
 
   if (!movePlayer) {
@@ -95,12 +91,12 @@ function onMovePlayer (data) {
   movePlayer.setY(data.y);
   movePlayer.setAngle(data.angle);
 
-  this.broadcast.emit('move player', {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY(), angle: movePlayer.getAngle()});
+  this.broadcast.emit('move player', { id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY(), angle: movePlayer.getAngle() });
 }
 
-function onShot(data){
-  let playerObj = playerById(this.id);
-  let self = this;
+function onShot(data) {
+  const playerObj = playerById(this.id);
+  const self = this;
   self.broadcast.emit(
     'shot',
     {
@@ -111,15 +107,38 @@ function onShot(data){
   );
 }
 
-function playerById (id, silence) {
-  let playerObj = players[id];
-  if(playerObj){
-    return playerObj;
-  }
-  else{
-    if(!silence){
-      util.log('Player not found: ', id);
-    }
-    return false;
-  }
+
+// New socket connection
+function onSocketConnection(client) {
+  util.log(`New player has connected: ${client.id}`);
+
+  // Listen for client disconnected
+  client.on('disconnect', onClientDisconnect);
+
+  // Listen for new player message
+  client.on('new player', onNewPlayer);
+
+  // Listen for move player message
+  client.on('move player', onMovePlayer);
+
+  // Listen for shot
+  client.on('shot', onShot);
+
+  client.on('kill', onKill);
 }
+
+const setEventHandlers = function () {
+  // Socket.IO
+  socket.sockets.on('connection', onSocketConnection);
+};
+
+module.exports = function init(server) {
+  // Create an empty object to store players
+  players = {};
+
+  // Attach Socket.IO to server
+  socket = io.listen(server);
+
+  // Start listening for events
+  setEventHandlers();
+};
